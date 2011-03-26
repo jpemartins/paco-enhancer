7faeb489ed5bc7787cf7fd62692ac03ab430324f
Joao Martins <madjackoo@gmail.com>
2011-03-26 01:54:25 +0000

    Historical Means
    - Popup only displaying current mean
    - Background already for data processing
    
    - Only missing:
      - Data Manip. , Graphic rendering

--- js/inject.js
+++ js/inject.js
@@ -1,3 +1,9 @@
+$.extend({
+    trim : function(str){
+        return str.replace(/^\s*/, "").replace(/\s*$/, "");
+    }
+});
+
 function Grade(i,n,y,s,u,v) {
     this.id = parseInt(i);
     this.name = n.replace(/^\s*/, '').replace(/\s*$/, '');
@@ -12,12 +18,24 @@ Grade.prototype.is_valid = function() {
         return false;
     }
     return true;
-};
+    };
 
 Grade.prototype.toString = function() {
     return sprintf('id=%d year=%d semester=%d ucts=%f value=%d', [self.id, self.year, self.semester, self.ucts, self.value]);
 };
 
+function HistoryLine(i,d,v) {
+    d = $.trim(d).split("-");
+    var _d = new Date(d[2], d[1], d[0]);
+    var months = [1,2,3,6,7,9,10];
+    this.id = parseInt(i);
+    this.date = _d;
+    this.year = _d.getFullYear();
+    if ( months.indexOf(_d.getMonth() + 1) ) {
+        this.year = _d.getFullYear() - 1;
+    }
+    this.value = parseInt(v);
+}
 
 function getDisciplinas() {
     var disciplines_html = $('.table_cell_par, .table_cell_impar');
@@ -30,18 +48,45 @@ function getDisciplinas() {
         }
         var grade = new Grade(_ds.eq(1).html(), _ds.eq(2).text(), _ds.eq(3).html(), _ds.eq(4).html(), _ds.eq(6).html(), _ds.eq(7).html());
         if (grade.is_valid()) {
-            this.grades.push(grade);
+            this.grades[grade.id] = grade;
         }
     }.bind(this));
     return this;
 }
 
-$(document).ready(function(evt) {
-    var disciplinas = getDisciplinas.bind({ grades: [] })();
+function getHistoryGrades() {
+    var disciplines_html = $("#history").contents().find('.table_cell_par, .table_cell_impar');
+    disciplines_html.each(function(index,value) {
+        var _ds = $('td', value);
+        var hline = new HistoryLine(_ds.eq(0).html(), _ds.eq(2).text(), _ds.eq(3).html());
+        if (this.history[hline.year] == undefined){ this.history[hline.year] = []; }
+        this.history[hline.year].push(hline);
+    }.bind(this));
+    return this;    
+}
+
+function fromLinesToGrades(grades, hlines){
+    var ret = {}
+    for (i in hlines){
+        var h = hlines[i].id;
+        if (grades[h]) {
+            ret[h] = grades[h];
+        }
+    }
+    return ret;
+}
+
+function calcMean() {
+    var disciplinas = getDisciplinas.bind({ grades: {} })();
     var grades = disciplinas.grades;
+    ret = _calcMeanSingle(grades).mean;
+    return { mean : ret, grades : grades, disciplinas: disciplinas }
+}
+
+function _calcMeanSingle(grades){
     var weights = 0, total_ects = 0;
     /* Calculate mean */
-    for (var i = 0; i < grades.length; ++i) {
+    for (var i in grades) {
         var g = grades[i];
         if (g.value) {
             weights += g.value * g.ects;
@@ -49,6 +94,46 @@ $(document).ready(function(evt) {
         }
     }
     var mean = weights / total_ects;
-    $('#template_main > table:eq(1) > tbody').append('<tr class=\"table_cell_impar\"><td align=\"left\">Media Ponderarada</td><td align=\"right\">'+ mean + '</td></tr>');
+    return { mean : mean, weights: weights, ects : total_ects};
+}
+
+function calcHistoryMeanAll(current_mean, grades){
+    $("body").append("<iframe id=\"history\" src=\"https://paco.ua.pt/secvirtual/c_historiconotas.asp\"></iframe>");
+    $("#history").load(function(evt){
+        var sent = {};
+        var data = getHistoryGrades.bind({ history: {} })();
+        var weights = 0, ects = 0;
+        for(year in data.history){
+            var hlines = data.history[year]; 
+            var mean = _calcMeanSingle(fromLinesToGrades(grades, hlines));
+            weights += mean.weights;
+            ects += mean.ects;
+            sent[year] = weights/ects;
+            renderYearMean(year, sent[year]);
+        }
+        sendHistoryMean(sent);
+        renderMean(current_mean);
+    }.bind(this));
+}
+
+/** Send to Extension */
+function sendToBase(data) { chrome.extension.sendRequest(data); }
+function sendMean(value){ sendToBase({ mean : value }); }
+function sendHistoryMean(values){ sendToBase({ history : values } ) }
+function sendGrades(values){ sendToBase({ grades : values } ) }
+
+/** Render */
+function renderMean(value){
+    $('#template_main > table:eq(1) > tbody').append('<tr class=\"table_cell_impar\"><td align=\"left\">Media Ponderada</td><td align=\"right\">'+ value.toFixed(2) + '</td></tr>');
+}
+
+function renderYearMean(year, value){
+    $('#template_main > table:eq(1) > tbody').append('<tr class=\"table_cell_impar\"><td align=\"left\">Media Ponderada ('+year+')</td><td align=\"right\">'+ value.toFixed(2) + '</td></tr>');
+}
+
+$(document).ready(function(evt) {
+    var curr = calcMean();
+    sendMean(curr.mean);
+    var history = calcHistoryMeanAll(curr.mean, curr.grades);
 });
 
